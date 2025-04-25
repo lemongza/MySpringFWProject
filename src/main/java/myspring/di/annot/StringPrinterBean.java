8feb4f97d697f9ba753550e72907e51e8a66fde0
lemongza <92498054+lemongza@users.noreply.github.com>
2025-04-25 16:46:23 +0900

    annot test

--- src/main/java/myspring/di/annot/StringPrinterBean.java
+++ src/main/java/myspring/di/annot/StringPrinterBean.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Component;
 @Component("stringPrinter")
 public class StringPrinterBean implements PrinterBean {
 	private StringBuffer buffer = new StringBuffer();
-	
+
 	public StringPrinterBean() {
 		System.out.println(this.getClass().getName() + " 기본생성자 호출됨!");
 	}
-	
+
 	public void print(String message) {
 		this.buffer.append(message);
 	}
